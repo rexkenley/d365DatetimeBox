1c09410c7aa2a00e6286efa553ddd1ecf074f1b3
rexkenley <rexkenley@hotmail.com>
2020-01-03 15:17:30 -0500

    Add Handlebar capable tooltip

--- src/jsx/datetimeBox.jsx
+++ src/jsx/datetimeBox.jsx
@@ -1,27 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Fabric } from "office-ui-fabric-react/lib/Fabric";
 import { Label } from "office-ui-fabric-react/lib/Label";
 import { DatePicker } from "office-ui-fabric-react";
 import { TooltipHost } from "office-ui-fabric-react/lib/Tooltip";
 import { getId } from "office-ui-fabric-react/lib/Utilities";
 import { initializeIcons } from "@uifabric/icons";
+import Handlebars from "handlebars";
+import HandlebarsIntl from "handlebars-intl";
 
 initializeIcons();
+HandlebarsIntl.registerWith(Handlebars);
 
 const datetimeBoxId = getId("datetimeBox"),
   tooltipId = getId("tooltip"),
   DatetimeBox = props => {
-    const { label, value, tooltip, onSelectDate } = props;
+    const { label, value, tooltip, onSelectDate } = props,
+      template = tooltip && Handlebars.compile(tooltip),
+      [content, setContent] = useState(
+        (template && value && template({ value })) || ""
+      );
 
     return (
       <Fabric style={{ display: "flex" }}>
         <Label htmlFor={datetimeBoxId}>{label}</Label>
-        <TooltipHost content={tooltip} id={tooltipId}>
+        <TooltipHost id={tooltipId} content={content}>
           <DatePicker
             id={datetimeBoxId}
             value={value}
+            onSelectDate={value => {
+              setContent((template && value && template({ value })) || "");
+              onSelectDate && onSelectDate(value);
+            }}
             placeholder="---"
-            onSelectDate={onSelectDate}
           />
         </TooltipHost>
       </Fabric>
--- src/jsx/datetimeBox.stories.js
+++ src/jsx/datetimeBox.stories.js
@@ -9,6 +9,12 @@ storiesOf("DatetimeBox", module)
   .add("With Hello label", () => <DatetimeBox label="Hello" />)
   .add("With 1/1/2020", () => <DatetimeBox value={new Date("1/1/2020")} />)
   .add("With Tooltip", () => <DatetimeBox tooltip="I am a tooltip" />)
+  .add("With Age Tooltip", () => (
+    <DatetimeBox
+      tooltip="Born {{formatRelative value units='year'}}"
+      value={new Date("1/1/1980")}
+    />
+  ))
   .add("With On Select Date Event", () => (
     <DatetimeBox onSelectDate={action("onSelectDate")} />
   ));
